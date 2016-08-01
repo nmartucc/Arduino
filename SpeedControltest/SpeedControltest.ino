d2df781cf16f2fcb61f5004b36cb3c15f2b3eb70
Rupesh Chinta <rupesh.ch97@gmail.com>
2016-08-01 12:52:51 -0400

    Speed Test File is now Working

--- SpeedControltest/SpeedControltest.ino
+++ SpeedControltest/SpeedControltest.ino
@@ -1,14 +1,17 @@
 int out = 11;
 int in = 0;
-float speedFactor = .8 // THIS SHOULD NEVER BE ABOVE 1
+float speedFactor = 1; // 1.2 mostly mirrors the input
+int speedLimit = 1.2; // Limit for the speed
 
 void setup() {
   pinMode(out, OUTPUT);
-  if (speedFactor > 1) {
-    speedFactor = 1;
+  Serial.begin(9600);
+  if (speedFactor > speedLimit) {
+    speedFactor = speedLimit;
   }
 }
 
 void loop() {
-  analogWrite(out, ((int) analogRead(in)*speedFactor/4));
+  analogWrite(out, (int) analogRead(in)*speedFactor/4);
+  Serial.println("HallEffect: " + String(analogRead(in), DEC) + "\t = " + String((int) ((float)analogRead(in)/1024*5000), DEC) + "mV\t Output: " + String(analogRead(2), DEC) + + " =\t" + String((int) ((float)analogRead(2)/1024*5000), DEC) + "mV");
 }
